1b00849b5c6b24784e0b62cf040acf523a7fb36d
Gretta Von Tobel <grettavt@gmail.com>
2021-08-12 15:33:14 -0700

    populate home page with popular movies

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { RestApiService } from 'src/app/services/rest-api.service';
 import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 import { trigger, state, style, animate, transition } from '@angular/animations';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
@@ -14,6 +15,31 @@ export class HomeComponent implements OnInit {
   Movie: any = []
 
   constructor( public restApi: RestApiService ) {}
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 600,
+    navText: ['&#8249', '&#8250;'],
+    responsive: {
+      0: {
+        items: 1 
+      },
+      400: {
+        items: 2
+      },
+      760: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    },
+    nav: true
+  }
 
   ngOnInit() {
     this.loadPopular()
